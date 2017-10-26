ee3b9ff2740582beedd0d7200eee85ade98b7c98
clarkenciel <clarkenciel@gmail.com>
2017-10-25 19:38:31 -0700

    feat(numeral): finished number parsing

--- src/numeral.rs
+++ src/numeral.rs
@@ -1,4 +1,6 @@
 use nom::*;
+use std::str::{self, FromStr};
+
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Numeral {
@@ -6,8 +8,41 @@ pub enum Numeral {
     Int(i32),
 }
 
-// named!(pub number<Numeral>,
-//        alt!(
-//            map_result!(
-//                do_parse!(prefix: digit >> dot: tag!(".") >> postfix: digit >> (prefix, dot, postfix)),
-//                |(pre, d, post)|
+impl From<i32> for Numeral {
+    fn from(num: i32) -> Numeral {
+        Numeral::Int(num)
+    }
+}
+
+impl From<f32> for Numeral {
+    fn from(num: f32) -> Numeral {
+        Numeral::Float(num)
+    }
+}
+
+named!(float<f32>,
+       map!(
+           recognize!(
+               tuple!(
+                   opt!(tag!("-")),
+                   digit,
+                   opt!(complete!(preceded!(tag!("."), digit)))
+               )
+           ),
+           |bs| str::from_utf8(bs).map(f32::from_str).unwrap().unwrap()
+       )
+);
+
+named!(int<i32>,
+       map!(
+           recognize!(
+               tuple!(
+                   opt!(tag!("-")),
+                   digit
+               )
+           ),
+           |bs| str::from_utf8(bs).map(|s| i32::from_str_radix(s, 10)).unwrap().unwrap()
+       )
+);
+
+named!(pub number<Numeral>, alt!(map!(int, Numeral::from) | map!(float, Numeral::from)));
