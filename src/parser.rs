b69994dcff7174f9339813a14d3dee8c26ab6918
clarkenciel <clarkenciel@gmail.com>
2017-11-06 21:03:12 -0800

    feat(parser): start to add bit ops

--- src/parser.rs
+++ src/parser.rs
@@ -4,26 +4,70 @@ use ops::*;
 use expr::Expr;
 use self::Expr::*;
 
-#[derive(Debug, PartialEq)]
-pub struct Exp(pub Box<Term>, pub Box<Exp1>);
+pub fn parse(input: &str) -> Result<Expr, String> {
+    exp(input.as_bytes()).to_result().map(Expr::from).map_err(
+        |e| {
+            format!("{}", e)
+        },
+    )
+}
 
 #[derive(Debug, PartialEq)]
-pub enum Exp1 {
+struct ShiftExp(Box<Exp>, Box<ShiftExp1>);
+
+#[derive(Debug, PartialEq)]
+enum ShiftExp1 {
+    Empty,
+    Seq(BitShift, Box<ShiftExp>),
+}
+
+#[derive(Debug, PartialEq)]
+struct BitAndExp(Box<ShiftExp>, Box<BitAndExp1>);
+
+#[derive(Debug, PartialEq)]
+enum BitAndExp1 {
+    Empty,
+    Seq(BitAnd, Box<BitAndExp>),
+}
+
+#[derive(Debug, PartialEq)]
+struct BitXOrExp(Box<BitAndExp>, Box<BitXOrExp1>);
+
+#[derive(Debug, PartialEq)]
+enum BitXOrExp1 {
+    Empty,
+    Seq(BitXOr, Box<BitXOrExp>),
+}
+
+#[derive(Debug, PartialEq)]
+struct BitOrExp(Box<BitXOrExp>, Box<BitOrExp1>);
+
+#[derive(Debug, PartialEq)]
+enum BitOrExp1 {
+    Empty,
+    Seq(BitOr, Box<BitOrExp>),
+}
+
+#[derive(Debug, PartialEq)]
+struct Exp(Box<Term>, Box<Exp1>);
+
+#[derive(Debug, PartialEq)]
+enum Exp1 {
     Empty,
     Seq(BinOp2, Box<Exp>),
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Term(pub Box<Factor>, pub Box<Term1>);
+struct Term(Box<Factor>, Box<Term1>);
 
 #[derive(Debug, PartialEq)]
-pub enum Term1 {
+enum Term1 {
     Empty,
     Seq(BinOp1, Box<Term>),
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Factor {
+enum Factor {
     Expr(Box<Expr>),
     Exp(Box<Exp>),
     Unary(UnOp, Box<Factor>),
@@ -34,12 +78,7 @@ impl From<Factor> for Expr {
         match f {
             Factor::Expr(expr) => *expr,
             Factor::Exp(exp) => Expr::from(*exp),
-            Factor::Unary(op, sub_f) => {
-                UnExpr(
-                    op,
-                    Box::new(Expr::from(*sub_f)),
-                )
-            }
+            Factor::Unary(op, sub_f) => UnExpr(op, Box::new(Expr::from(*sub_f))),
         }
     }
 }
@@ -48,11 +87,13 @@ impl From<Term> for Expr {
     fn from(t: Term) -> Self {
         match *t.1 {
             Term1::Empty => Expr::from(*t.0),
-            Term1::Seq(op, t2) => BinExpr(
-                Box::new(Expr::from(*t.0)),
-                BinOp::One(op),
-                Box::new(Expr::from(*t2))
-            )
+            Term1::Seq(op, t2) => {
+                BinExpr(
+                    Box::new(Expr::from(*t.0)),
+                    BinOp::One(op),
+                    Box::new(Expr::from(*t2)),
+                )
+            }
         }
     }
 }
@@ -61,48 +102,143 @@ impl From<Exp> for Expr {
     fn from(e: Exp) -> Self {
         match *e.1 {
             Exp1::Empty => Expr::from(*e.0),
-            Exp1::Seq(op, e2) => BinExpr(
-                Box::new(Expr::from(*e.0)),
-                BinOp::Two(op),
-                Box::new(Expr::from(*e2))
-            ),
+            Exp1::Seq(op, e2) => {
+                BinExpr(
+                    Box::new(Expr::from(*e.0)),
+                    BinOp::Two(op),
+                    Box::new(Expr::from(*e2)),
+                )
+            }
         }
     }
 }
 
-pub fn parse(input: &str) -> Result<Expr, String> {
-    exp(input.as_bytes())
-        .to_result()
-        .map(Expr::from)
-        .map_err(|e| format!("{}", e))
+impl From<ShiftExp> for Expr {
+    fn from(e: ShiftExp) -> Self {
+        match *e.1 {
+            ShiftExp1::Empty => Expr::from(*e.0),
+            ShiftExp1::Seq(op, e2) => {
+                BinExpr(
+                    Box::new(Expr::from(*e.0)),
+                    BinOp::Three(op),
+                    Box::new(Expr::from(*e2)),
+                )
+            }
+        }
+    }
+}
+
+impl From<BitAndExp> for Expr {
+    fn from(e: BitAndExp) -> Self {
+        match *e.1 {
+            BitAndExp1::Empty => Expr::from(*e.0),
+            BitAndExp1::Seq(op, e2) => {
+                BinExpr(
+                    Box::new(Expr::from(*e.0)),
+                    BinOp::Four(op),
+                    Box::new(Expr::from(*e2)),
+                )
+            }
+        }
+    }
+}
+
+impl From<BitXOrExp> for Expr {
+    fn from(e: BitXOrExp) -> Self {
+        match *e.1 {
+            BitXOrExp1::Empty => Expr::from(*e.0),
+            BitXOrExp1::Seq(op, e2) => {
+                BinExpr(
+                    Box::new(Expr::from(*e.0)),
+                    BinOp::Five(op),
+                    Box::new(Expr::from(*e2)),
+                )
+            }
+        }
+    }
+}
+
+impl From<BitOrExp> for Expr {
+    fn from(e: BitOrExp) -> Self {
+        match *e.1 {
+            BitOrExp1::Empty => Expr::from(*e.0),
+            BitOrExp1::Seq(op, e2) => {
+                BinExpr(
+                    Box::new(Expr::from(*e.0)),
+                    BinOp::Six(op),
+                    Box::new(Expr::from(*e2)),
+                )
+            }
+        }
+    }
 }
 
 named!(time<Expr>, value!(Time, alt!(char!('T') | char!('t'))));
 named!(num<Expr>, map!(number, Num));
 
-named!(pub exp<Exp>, ws!(
+named!(bit_or_exp<BitOrExp>, ws!(
+    map!(pair!(bit_xor_exp, bit_or_exp1), |(e, se)| BitOrExp(Box::new(e), Box::new(se)))
+));
+
+named!(bit_or_exp1<BitOrExp1>, ws!(alt!(
+    value!(BitOrExp1::Empty, empty) |
+    map!(pair!(bit_or, bit_or_exp), |(o, e)| BitOrExp1::Seq(o, Box::new(e))) |
+    value!(BitOrExp1::Empty, tag!(""))
+)));
+
+named!(bit_xor_exp<BitXOrExp>, ws!(
+    map!(pair!(bit_and_exp, bit_xor_exp1), |(e, se)| BitXOrExp(Box::new(e), Box::new(se)))
+));
+
+named!(bit_xor_exp1<BitXOrExp1>, ws!(alt!(
+    value!(BitXOrExp1::Empty, empty) |
+    map!(pair!(bit_xor, bit_xor_exp), |(o, e)| BitXOrExp1::Seq(o, Box::new(e))) |
+    value!(BitXOrExp1::Empty, tag!(""))
+)));
+
+named!(bit_and_exp<BitAndExp>, ws!(
+    map!(pair!(shift_exp, bit_and_exp1), |(e, se)| BitAndExp(Box::new(e), Box::new(se)))
+));
+
+named!(bit_and_exp1<BitAndExp1>, ws!(alt!(
+    value!(BitAndExp1::Empty, empty) |
+    map!(pair!(bit_and, bit_and_exp), |(o, e)| BitAndExp1::Seq(o, Box::new(e))) |
+    value!(BitAndExp1::Empty, tag!(""))
+)));
+
+named!(shift_exp<ShiftExp>, ws!(
+    map!(pair!(exp, shift_exp1), |(e, se)| ShiftExp(Box::new(e), Box::new(se)))
+));
+
+named!(shift_exp1<ShiftExp1>, ws!(alt!(
+    value!(ShiftExp1::Empty, empty) |
+    map!(pair!(bit_shift, shift_exp), |(o, e)| ShiftExp1::Seq(o, Box::new(e))) |
+    value!(ShiftExp1::Empty, tag!(""))
+)));
+
+named!(exp<Exp>, ws!(
     map!(pair!(term, exp1), |(t, e)| Exp(Box::new(t), Box::new(e)))
 ));
 
-named!(pub exp1<Exp1>, ws!(alt!(
+named!(exp1<Exp1>, ws!(alt!(
     value!(Exp1::Empty, empty) |
     map!(pair!(add_or_sub, exp), |(o, e)| Exp1::Seq(o, Box::new(e))) |
     value!(Exp1::Empty, tag!(""))
 )));
 
-named!(pub term<Term>, ws!(
+named!(term<Term>, ws!(
     map!(pair!(factor, term1), |(f, t)| Term(Box::new(f), Box::new(t)))
 ));
 
 named!(empty<&[u8]>, alt!(eof!()));
 
-named!(pub term1<Term1>, ws!(alt!(
+named!(term1<Term1>, ws!(alt!(
     value!(Term1::Empty, empty) |
     map!(pair!(mul_or_div, term), |(o, t)| Term1::Seq(o, Box::new(t))) |
     value!(Term1::Empty, tag!(""))
 )));
 
-named!(pub factor<Factor>, ws!(
+named!(factor<Factor>, ws!(
     alt!(
         map!(
             alt!(time | num),
--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -1,319 +1,151 @@
 extern crate bbb_core;
-extern crate nom;
 
 use bbb_core::parser::*;
 use bbb_core::expr::Expr::*;
 use bbb_core::numeral::Numeral::*;
-use bbb_core::ops;
+use bbb_core::ops::*;
 use bbb_core::ops::UnOp::*;
-use nom::IResult::*;
-
-// #[test]
-// fn time_parse() {
-//     let exp1 = "t";
-//     let exp2 = " t";
-//     let exp3 = "t ";
-
-//     assert_eq!(parse(exp1), Ok(Box::new(Time)));
-//     assert_eq!(parse(exp2), Ok(Box::new(Time)));
-//     assert_eq!(parse(exp3), Ok(Box::new(Time)));
-
-//     let exp1 = "T";
-//     let exp2 = " T";
-//     let exp3 = "T ";
-//     assert_eq!(parse(exp1), Ok(Box::new(Time)));
-//     assert_eq!(parse(exp2), Ok(Box::new(Time)));
-//     assert_eq!(parse(exp3), Ok(Box::new(Time)));
-// }
-
-// #[test]
-// fn number_parse() {
-//     let exp1 = "10";
-//     assert_eq!(parse(exp1), Ok(Box::new(Num(Int(10)))));
-
-//     let exp2 = "10.1";
-//     assert_eq!(parse(exp2), Ok(Box::new(Num(Float(10.1)))));
-
-//     let exp3 = "-10";
-//     assert_eq!(parse(exp3), Ok(Box::new(Num(Int(-10)))));
-
-//     let exp4 = "-10.1";
-//     assert_eq!(parse(exp4), Ok(Box::new(Num(Float(-10.1)))));
-
-//     let exp5 = "0.1";
-//     assert_eq!(parse(exp5), Ok(Box::new(Num(Float(0.1)))));
-
-//     let exp6 = "-0.1";
-//     assert_eq!(parse(exp6), Ok(Box::new(Num(Float(-0.1)))));
-// }
 
 #[test]
-fn empty_exp1_test() {
-    let e = "".as_bytes();
+fn number_parse_test() {
+    let e = "1";
     assert_eq!(
-        exp1(e),
-        Done(&b""[..],
-             Exp1::Empty)
+        parse(e),
+        Ok(Num(Int(1)))
+    );
+
+    let e = "-1";
+    assert_eq!(
+        parse(e),
+        Ok(Num(Int(-1)))
+    );
+
+    let e = " -10 ";
+    assert_eq!(
+        parse(e),
+        Ok(Num(Int(-10)))
+    );
+
+    let e = "(-10)";
+    assert_eq!(
+        parse(e),
+        Ok(Num(Int(-10)))
     );
 }
 
 #[test]
-fn seq_exp1_test() {
-    let e = "+ 1".as_bytes();
+fn time_parse_test() {
+    let e = "t";
     assert_eq!(
-        exp1(e),
-        Done(&b""[..],
-             Exp1::Seq(
-                 ops::BinOp2::Add,
-                 Box::new(Exp(
-                     Box::new(Term(
-                         Box::new(Factor::Expr(Box::new(Num(Int(1))))),
-                         Box::new(Term1::Empty)
-                     )),
-                     Box::new(Exp1::Empty))
-                 )
-             ))
+        parse(e),
+        Ok(Time)
+    );
+
+    let e = " t ";
+    assert_eq!(
+        parse(e),
+        Ok(Time)
+    );
+
+    let e = "(t)";
+    assert_eq!(
+        parse(e),
+        Ok(Time)
     );
 }
 
 #[test]
-fn number_factor_test() {
-    let e = "1".as_bytes();
+fn unary_parse_test() {
+    let e = "-(10)";
     assert_eq!(
-        factor(e),
-        Done(&b""[..],
-             Factor::Expr(Box::new(Num(Int(1))))
+        parse(e),
+        Ok(UnExpr(Neg, Box::new(Num(Int(10)))))
+    );
+
+    let e = "~10";
+    assert_eq!(
+        parse(e),
+        Ok(UnExpr(BitNot, Box::new(Num(Int(10)))))
+    );
+
+    let e = "!10";
+    assert_eq!(
+        parse(e),
+        Ok(UnExpr(BoolNot, Box::new(Num(Int(10)))))
+    );
+}
+
+#[test]
+fn simple_binary_parse_test() {
+    let e = "1 + 1";
+    assert_eq!(
+        parse(e),
+        Ok(
+            BinExpr(
+                Box::new(Num(Int(1))),
+                BinOp::Two(BinOp2::Add),
+                Box::new(Num(Int(1)))
+            )
         )
     );
 
-    let e = "-1".as_bytes();
+    let e = "1 * 1";
     assert_eq!(
-        factor(e),
-        Done(&b""[..],
-             Factor::Expr(Box::new(Num(Int(-1))))
+        parse(e),
+        Ok(
+            BinExpr(
+                Box::new(Num(Int(1))),
+                BinOp::One(BinOp1::Mul),
+                Box::new(Num(Int(1)))
+            )
         )
     );
-}
 
-#[test]
-fn time_factor_test() {
-    let e = "t".as_bytes();
+    let e = "1 >> 1";
     assert_eq!(
-        factor(e),
-        Done(&b""[..],
-             Factor::Expr(Box::new(Time)))
-    );
-}
-
-#[test]
-fn unary_factor_test() {
-    let e = "-t".as_bytes();
-    assert_eq!(
-        factor(e),
-        Done(&b""[..],
-             Factor::Unary(Neg, Box::new(Factor::Expr(Box::new(Time)))))
-    );
-
-    let e = "-(1)".as_bytes();
-    assert_eq!(
-        factor(e),
-        Done(&b""[..],
-             Factor::Unary(
-                 Neg,
-                 Box::new(Factor::Exp(
-                     Box::new(Exp(
-                         Box::new(Term(
-                             Box::new(Factor::Expr(
-                                 Box::new(Num(Int(1)))
-                             )),
-                             Box::new(Term1::Empty)
-                         )),
-                         Box::new(Exp1::Empty)
-                     ))
-                 ))
-             )
+        parse(e),
+        Ok(
+            BinExpr(
+                Box::new(Num(Int(1))),
+                BinOp::Three(BitShift::Right),
+                Box::new(Num(Int(1)))
+            )
         )
     );
-}
 
-#[test]
-fn empty_term1_test() {
-    let e = "".as_bytes();
+    let e = "1 & 1";
     assert_eq!(
-        term1(e),
-        Done(&b""[..], Term1::Empty)
-    );
-}
-
-#[test]
-fn seq_term1_test() {
-    let e = "* 10".as_bytes();
-    assert_eq!(
-        term1(e),
-        Done(&b""[..],
-             Term1::Seq(
-                 ops::BinOp1::Mul,
-                 Box::new(Term(
-                     Box::new(Factor::Expr(
-                         Box::new(Num(Int(10)))
-                     )),
-                     Box::new(Term1::Empty)
-                 ))
-             )
+        parse(e),
+        Ok(
+            BinExpr(
+                Box::new(Num(Int(1))),
+                BinOp::Four(BitAnd),
+                Box::new(Num(Int(1)))
+            )
         )
     );
-}
 
-#[test]
-fn number_exp_test() {
-    let e = "1".as_bytes();
+    let e = "1 ^ 1";
     assert_eq!(
-        exp(e),
-        Done(&b""[..],
-             Exp(Box::new(Term(Box::new(Factor::Expr(Box::new(Num(Int(1))))),
-                               Box::new(Term1::Empty))),
-                 Box::new(Exp1::Empty)
-             )
+        parse(e),
+        Ok(
+            BinExpr(
+                Box::new(Num(Int(1))),
+                BinOp::Five(BitXOr),
+                Box::new(Num(Int(1)))
+            )
         )
     );
-}
 
-#[test]
-fn add_exp_test() {
-    let e = "1 + 1".as_bytes();
-
+    let e = "1 | 1";
     assert_eq!(
-        exp(e),
-        Done(&b""[..],
-             Exp(
-                 Box::new(Term(
-                     Box::new(Factor::Expr(
-                         Box::new(Num(Int(1)))
-                     )),
-                     Box::new(Term1::Empty)
-                 )),
-                 Box::new(Exp1::Seq(
-                     ops::BinOp2::Add,
-                     Box::new(Exp(
-                         Box::new(Term(
-                             Box::new(Factor::Expr(
-                                 Box::new(Num(Int(1)))
-                             )),
-                             Box::new(Term1::Empty)
-                         )),
-                         Box::new(Exp1::Empty)
-                     ))
-                 ))
-             )
-        )
-    );
-}
-
-#[test]
-fn mul_exp_test() {
-    let e = "1 * 1".as_bytes();
-
-    assert_eq!(
-        exp(e),
-        Done(&b""[..],
-             Exp(
-                 Box::new(Term(
-                     Box::new(Factor::Expr(
-                         Box::new(Num(Int(1)))
-                     )),
-                     Box::new(Term1::Seq(
-                         ops::BinOp1::Mul,
-                         Box::new(Term(
-                             Box::new(Factor::Expr(
-                                 Box::new(Num(Int(1)))
-                             )),
-                             Box::new(Term1::Empty)
-                         ))
-                     ))
-                 )),
-                 Box::new(Exp1::Empty)
-             )
-        )
-    );
-}
-
-#[test]
-fn three_term_test() {
-    let e = "1 + 1 + 1".as_bytes();
-
-    assert_eq!(
-        exp(e),
-        Done(&b""[..],
-             Exp(
-                 Box::new(Term(
-                     Box::new(Factor::Expr(
-                         Box::new(Num(Int(1)))
-                     )),
-                     Box::new(Term1::Empty)
-                 )),
-                 Box::new(Exp1::Seq(
-                     ops::BinOp2::Add,
-                     Box::new(Exp(
-                         Box::new(Term(
-                             Box::new(Factor::Expr(
-                                 Box::new(Num(Int(1)))
-                             )),
-                             Box::new(Term1::Empty)
-                         )),
-                         Box::new(Exp1::Seq(
-                             ops::BinOp2::Add,
-                             Box::new(Exp(
-                                 Box::new(Term(
-                                     Box::new(Factor::Expr(
-                                         Box::new(Num(Int(1)))
-                                     )),
-                                     Box::new(Term1::Empty)
-                                 )),
-                                 Box::new(Exp1::Empty)
-                             ))
-                         ))
-                     ))
-                 ))
-             )
-        )
-    );
-}
-
-#[test]
-fn mixed_exp_test() {
-    let e = "1 * 1 + 1".as_bytes();
-
-    assert_eq!(
-        exp(e),
-        Done(&b""[..],
-             Exp(
-                 Box::new(Term(
-                     Box::new(Factor::Expr(
-                         Box::new(Num(Int(1)))
-                     )),
-                     Box::new(Term1::Seq(
-                         ops::BinOp1::Mul,
-                         Box::new(Term(
-                             Box::new(Factor::Expr(
-                                 Box::new(Num(Int(1)))
-                             )),
-                             Box::new(Term1::Empty)
-                         ))
-                     ))
-                 )),
-                 Box::new(Exp1::Seq(
-                     ops::BinOp2::Add,
-                     Box::new(Exp(
-                         Box::new(Term(
-                             Box::new(Factor::Expr(
-                                 Box::new(Num(Int(1)))
-                             )),
-                             Box::new(Term1::Empty)
-                         )),
-                         Box::new(Exp1::Empty)
-                     ))
-                 ))
-             )
+        parse(e),
+        Ok(
+            BinExpr(
+                Box::new(Num(Int(1))),
+                BinOp::Six(BitOr),
+                Box::new(Num(Int(1)))
+            )
         )
     );
 }
